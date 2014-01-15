6ba1df9aa339aa41492a7d8723745bccad3bbb9c
Matt Bornski <matt@bornski.com>
2014-01-15 08:49:27 -0800

    Add an option to permit writing the *same* value back

--- lib/can_has_validations/validators/write_once_validator.rb
+++ lib/can_has_validations/validators/write_once_validator.rb
@@ -1,8 +1,12 @@
 # write-once, read-many
-#   Allows a value to be set to a non-nil value once, and then makes it immutable.
-#   Combine with :existence=>true to accomplish the same thing as attr_readonly,
-#   except with error messages (instead of silently refusing to save the change).
+# Allows a value to be set to a non-nil value once, and then makes it immutable.
+# Combine with :existence=>true to accomplish the same thing as attr_readonly,
+# except with error messages (instead of silently refusing to save the change).
 # eg: validates :user_id, :write_once=>true
+# 
+# Can also be configured to ignore identical writes of the already set value,
+# useful in blind-update operations.
+# eg: validates :user_id, :write_once=>{:ignore_identical=>true}
 
 module ActiveModel::Validations
   class WriteOnceValidator < ActiveModel::EachValidator
@@ -16,8 +20,12 @@ module ActiveModel::Validations
     end
 
     def validate_each(record, attribute, value)
+      ignore_identical = options[:ignore_identical] || false
       if record.persisted? && record.send("#{attribute}_changed?") && !record.send("#{attribute}_was").nil?
-        record.errors.add(attribute, :unchangeable, options)
+        # The attribute has changed from a non-nil to a non-nil
+        if !ignore_identical || value == record.send("#{attribute}_was")
+          record.errors.add(attribute, :unchangeable, options)
+        end
       end
     end
   end
